617a51ee13ce6bc9b9c8cf2485533fccc65071f1
u1986970 <u1986970@campus.udg.edu>
2024-03-04 17:21:24 +0100

    canvis 1

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,9 @@
-addEventListener('load', function() {
-    document.getElementById('play').addEventListener('click', 
-    function(){
-        window.location.assign("./html/game.html");
-    });
+var play=$('#play');
+var options=$('#options');
+var saves=$('#saves');
+var exit=$('#exit');
 
-    document.getElementById('options').addEventListener('click', 
-    function(){
-        window.location.assign("./html/options.html");
-    });
-
-    document.getElementById('saves').addEventListener('click', 
-    function(){
-        console.error("Opció no implementada");
-    });
-
-    document.getElementById('exit').addEventListener('click', 
-    function(){
-        console.warn("No es pot sortir!");
-    });
-});
+play.on('click', function() { window.location.assign("./html/game.html");})
+options.on('click', function() { window.location.assign("./html/options.html");})
+saves.on('click', function() { console.error("Opció no implementada");})    
+exit.on('click', function() { console.warn("No es pot sortir!");})
