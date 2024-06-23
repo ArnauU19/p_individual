8493de2a646a354aaef8bd7b989877e70b4a8375
u1986970 <u1986970@campus.udg.edu>
2024-06-23 19:15:04 +0200

    all fine

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ import { game as gController } from "./memory.js";
 
 var game = $('#game');
 
+$('#save').on('click', ()=>gController.save());
+
 gController.init(updateSRC).forEach(function(card, indx){
     game.append('<img id="c'+indx+'" class="card" title="card">');
     card.pointer = $('#c'+indx);
--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,30 @@
 
-$('#play').on('click',function(){
-    window.location.assign("./html/game.html");
-});
-$('#options').on('click',function(){
-    window.location.assign("./html/options.html");
-});
-$('#saves').on('click',function(){
-    console.error("Opció no implementada");
-});
-$('#exit').on('click',function(){
-    console.warn("No es pot sortir!");
+addEventListener('load', function() {
+    $('#play').on('click',function(){
+        sessionStorage.removeItem("save");
+        window.location.assign("./html/game.html");
+    });
+
+    $('#saves').on('click',function(){
+        fetch("./php/load.php",{
+            method: "POST",
+            body: "",
+            headers: {"content-type":"application/json; charset=UTF-8"}
+        })
+        .then(response=>{
+            if (response.ok) response.text();
+            else trow("PHP connection fail");
+        })
+        .then(partida=>sessionStorage.save = partida)
+        .catch(err=>sessionStorage.save = localStorage.save)
+        .finally(()=>window.location.assign("./html/game.html"));
+
+    });
+
+    $('#options').on('click',function(){
+        window.location.assign("./html/options.html");
+    });
+    $('#exit').on('click',function(){
+        console.warn("No es pot sortir!");
+    });
 });
