717e7db0abbf2a4bb0be0431c9de301dbde1ad3e
u1986970 <u1986970@campus.udg.edu>
2024-06-23 19:13:41 +0200

    aaaaaa

---
+++ library/uuidlib.js
@@ -0,0 +1,10 @@
+export const random_uuid = uuidv4();
+ 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    .replace(/[xy]/g, function (c) {
+        const r = Math.random() * 16 | 0, 
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
