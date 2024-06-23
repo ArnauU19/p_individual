27ad681b3ad6399c422a5f29ddd57a15768f500a
u1986970 <u1986970@campus.udg.edu>
2024-06-23 21:01:48 +0200

    aaaaaa

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,13 @@
 
-addEventListener('load', function() {
-    $('#play').on('click',function(){
-        sessionStorage.removeItem("save");
-        window.location.assign("./html/game.html");
-    });
-
-    $('#saves').on('click',function(){
-        fetch("./php/load.php",{
-            method: "POST",
-            body: "",
-            headers: {"content-type":"application/json; charset=UTF-8"}
-        })
-        .then(response=>{
-            if (response.ok) response.text();
-            else trow("PHP connection fail");
-        })
-        .then(partida=>sessionStorage.save = partida)
-        .catch(err=>sessionStorage.save = localStorage.save)
-        .finally(()=>window.location.assign("./html/game.html"));
-
-    });
-
-    $('#options').on('click',function(){
-        window.location.assign("./html/options.html");
-    });
-    $('#exit').on('click',function(){
-        console.warn("No es pot sortir!");
-    });
+$('#play').on('click',function(){
+    window.location.assign("./html/game.html");
+});
+$('#options').on('click',function(){
+    window.location.assign("./html/options.html");
+});
+$('#saves').on('click',function(){
+    console.error("Opció no implementada");
+});
+$('#exit').on('click',function(){
+    console.warn("No es pot sortir!");
 });
